d1b6793a78b96e12f763d32cf090646eaadab6b4
viniciusssantos <vinicius.santos@lejour.com.br>
2020-10-13 19:58:56 -0300

    Added CRUD to product service

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CrudService } from 'src/lib/crud-services/crud-services';
+import { Repository } from 'typeorm';
+import { Product } from './product.entity';
 
 @Injectable()
-export class ProductService {}
+export class ProductService extends CrudService<Product> {
+  constructor(@InjectRepository(Product) repo: Repository<Product> ) {
+    super(repo);
+  }
+}
